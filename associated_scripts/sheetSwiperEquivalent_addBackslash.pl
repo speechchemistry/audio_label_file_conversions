7f91c17ce7d6ed8e71a34d82da7826c44aec7f83
speechchemistry <33318937+speechchemistry@users.noreply.github.com>
2024-07-24 12:43:59 +0100

    Update sheetSwiperEquivalent_addBackslash.pl
    Added comment about LF terminators requirement

--- associated_scripts/sheetSwiperEquivalent_addBackslash.pl
+++ associated_scripts/sheetSwiperEquivalent_addBackslash.pl
@@ -2,9 +2,10 @@
 # this script implements the equivalent of sheetswiper except
 # that it adds the backslash prefix to column headings
 
-# input: the tsv file (excel might have added quotes around text
+# input: the tsv file (Might require LF terminators only. 
+#        Excel might have added quotes around text
 #        containing commas which you probably should remove as
-#        it leads to errors in silpa)
+#        it leads to errors in silpa.)
 # output: the toolbox file
 
 @ARGV = glob("@ARGV") if ($^O eq 'MSWin32');  
