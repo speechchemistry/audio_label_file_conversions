33004c36994347144c9262f5b5de3c8fccd9ae09
speechchemistry <33318937+speechchemistry@users.noreply.github.com>
2025-05-23 16:31:11 +0100

    Update sheetSwiperEquivalent_addBackslash.pl
    Don't print out empty fields in the SFM. This small change was made with help from ChatGPT

--- associated_scripts/sheetSwiperEquivalent_addBackslash.pl
+++ associated_scripts/sheetSwiperEquivalent_addBackslash.pl
@@ -2,10 +2,9 @@
 # this script implements the equivalent of sheetswiper except
 # that it adds the backslash prefix to column headings
 
-# input: the tsv file (Might require LF terminators only. 
-#        Excel might have added quotes around text
+# input: the tsv file (excel might have added quotes around text
 #        containing commas which you probably should remove as
-#        it leads to errors in silpa.)
+#        it leads to errors in silpa)
 # output: the toolbox file
 
 @ARGV = glob("@ARGV") if ($^O eq 'MSWin32');  
@@ -16,7 +15,6 @@ while(<>) # go through the feature csv file and put into the arrays
    my $line = $_;
    $line =~ s/\r?\n$//; # chomp LF or CRLF terminators
    @lineArray = split(/\t/,$line); 
-   #   $phone = shift @lineArray;
    if ($count==0)
    {
       @labelArray = @lineArray;      
@@ -25,10 +23,11 @@ while(<>) # go through the feature csv file and put into the arrays
    {
        print "\n";
        $countElement = 0;
-       for $lineArrayElement (@lineArray)
-       {
-	  print "\\$labelArray[$countElement] $lineArrayElement\n";
-          $countElement++;
+       for my $lineArrayElement (@lineArray) {
+         if ($lineArrayElement ne '') {
+            print "\\$labelArray[$countElement] $lineArrayElement\n";
+         }
+       $countElement++;
        }
    } 
    $count++;
